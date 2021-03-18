7cb87f4e877b9f0e2bac68cc2c873f06ab598209
seismicsans201X <jrfmathis06@outlook.com>
2021-03-18 16:17:00 +0000

    added user input for time on poll

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
-from replit import db
 import os
-import time
 import requests
 import randfacts
 from pyrandmeme import *
@@ -45,9 +43,27 @@ async def meme(ctx):
   await ctx.send(embed=await pyrandmeme())
 
 @bot.command(
-  help='Creates a poll in the channel called "voting". There must be a voting channel for this to work'
+  help='Creates a poll in the channel called "voting". There must be a voting channel for this to work. If the issue is more than one word long, you must put it in quotes "like this." The time unit defaults to being seconds, but you can specify it to be minutes, hours, or days. They must be spelled out completely and correctly, but capitalization does not matter'
 )
-async def poll(ctx, *, issue):
+async def poll(ctx, issue, timeLimit, timeUnit='seconds'):
+  try:
+    timeLimit = float(timeLimit)
+  except ValueError:
+    await ctx.send('The time limit must be a number!')
+    return
+
+  if timeUnit.upper() == 'SECONDS':
+    pass
+  elif timeUnit.upper() == 'MINUTES':
+    timeLimit *= 60
+  elif timeUnit.upper() == 'HOURS':
+    timeLimit *= 3600
+  elif timeUnit.upper() == 'DAYS':
+    timeLimit *= 86400
+  else:
+    await ctx.send('Time unit invalid! Valid time units are minutes, hours, and days!')
+    return
+
   vote_channel = discord.utils.get(ctx.guild.channels, name='voting')
 
   await ctx.message.delete()
@@ -61,15 +77,15 @@ async def poll(ctx, *, issue):
     return user != '808555253317894163' and str(reaction.emoji) in ['👍', '👎']
 
   yay = 0
-  nay = -1
+  nay = 0
   loop = 0
 
   while loop == 0:
     try:
-      reaction, user = await bot.wait_for('reaction_add', timeout=10.0, check=check)
+      reaction, user = await bot.wait_for('reaction_add', timeout=timeLimit, check=check)
       if reaction.emoji == '👍' and user != '808555253317894163':
         yay += 1
-      elif reaction.emoji == '👎' and user != '808555253317894163':
+      if reaction.emoji == '👎' and user != '808555253317894163':
         nay += 1
       
     except:
