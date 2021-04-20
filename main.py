7401abb2432eecf016eb9e45643d496cc505915a
seismicsans201X <>
2021-04-20 12:08:33 +0000

    Added suggestion command

--- main.py
+++ main.py
@@ -42,14 +42,20 @@ async def changePrefix(ctx, prefix):
   await ctx.send(f'Prefix changed to {prefix}!')
 
 @bot.command(
-  name='bugReport',
   help='Use this to send me a bug report/tell me if the bot is offline. When sending a bug report, please tell me what command you ran than made the bug happen, as well as any messages the bot sent, if any. I may DM you about it if I need more information.'
 )
 async def bugReport(ctx, *, bug):
   me = await bot.fetch_user(506915906568585226)
-  await me.send(bug)
+  await me.send(f'New bug report: {bug}')
   reporter = await bot.fetch_user(ctx.author.id)
   await me.send(f'Report submitted by: {reporter}')
+
+@bot.command()
+async def suggestion(ctx, *, suggestion):
+  me = await bot.fetch_user(506915906568585226)
+  await me.send(f'New suggestion: {suggestion}')
+  reporter = await bot.fetch_user(ctx.author.id)
+  await me.send(f'Suggestion submitted by: {reporter}')
 
 @bot.command(
   help='Sends an inspiring quote'
